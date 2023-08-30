791989cccb6fe28d91370c67855a18afecba2cbd
FJrodafo <cnry.fjra@gmail.com>
2023-08-30 14:48:49 +0100

    v15

--- App/src/commands/moderation/kick.js
+++ App/src/commands/moderation/kick.js
@@ -1,13 +1,13 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('kick')
-		.setDescription('Select a member and kick them (but not really).')
+    data: new SlashCommandBuilder()
+        .setName('kick')
+        .setDescription('Select a member and kick them (but not really).')
         .setDefaultMemberPermissions(0)
-		.addUserOption(option => option.setName('target').setDescription('The member to kick').setRequired(true)),
-	async execute(interaction) {
-		const member = interaction.options.getMember('target');
-		return interaction.reply({ content: `You wanted to kick: ${member.user.username}`, ephemeral: true });
-	},
+        .addUserOption(option => option.setName('target').setDescription('The member to kick').setRequired(true)),
+    async execute(interaction) {
+        const member = interaction.options.getMember('target');
+        return interaction.reply({ content: `You wanted to kick: ${member.user.username}`, ephemeral: true });
+    },
 };
