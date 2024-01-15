7f341c2a7aaf7ea2f381595d784c9f4c7b9e8503
FJrodafo <cnry.fjra@gmail.com>
2024-01-15 14:56:56 +0000

    v33

--- App/src/commands/fun/ping.js
+++ App/src/commands/fun/ping.js
@@ -3,10 +3,12 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Replies with Pong!'),
+        .setDescription('🏓 Replies with Pong!'),
     async execute(interaction) {
         // const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+        const sent = await interaction.reply({ content: 'pinging...', fetchReply: true });
         // interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
-        return interaction.reply('Pong!');
+        interaction.editReply(`:ping_pong: pong **${sent.createdTimestamp - interaction.createdTimestamp}**ms`);
+        // return interaction.reply('Pong!');
     },
 };
